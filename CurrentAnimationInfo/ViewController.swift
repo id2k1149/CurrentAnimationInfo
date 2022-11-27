e7faa5a4a60374b8a8cdb440b9331d820d6e0d15
id2k1149 <id2k1149@gmail.com>
2022-11-27 12:26:49 -0500

    Initial Commit

---
+++ CurrentAnimationInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrentAnimationInfo
+//
+//  Created by Max Franz Immelmann on 11/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
